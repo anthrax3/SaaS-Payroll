265e5bedfcadcb14e7446c2320e862abf497c6e2
Pasan Mudalige <pasanmudalige9@gmail.com>
2017-06-24 15:21:35 +0530

    BLL Contracts

---
+++ OpenQbit.PayrollSystem.git/OpenQbit.Payroll.BLL.BusinessService.Contracts/IBonusManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Payroll.Common.Models;
+
+namespace OpenQbit.Payroll.BLL.BusinessService.Contracts
+{
+    public interface IBonusManager
+    {
+        bool RecordBonus(Bonus bonus);
+    }
+}
---
+++ OpenQbit.PayrollSystem.git/OpenQbit.Payroll.BLL.BusinessService/AdvanceManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQbit.Payroll.BLL.BusinessService.Contracts;
+using OpenQbit.Payroll.Common.Models;
+
+namespace OpenQbit.Payroll.BLL.BusinessService
+{
+    public class AdvanceManager : IAdvanceManager
+    {
+        public bool RecordAdvance(Advance advance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
