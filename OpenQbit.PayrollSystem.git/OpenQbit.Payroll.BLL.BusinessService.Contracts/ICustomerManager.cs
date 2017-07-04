f09b5666deb4dc828259557f98cc727856648410
Sandamal Wijeweera <sandamalwijeweera@gmail.com>
2017-07-04 15:45:59 +0530

    Implementing Busoness Logics of System and Customer

---
+++ OpenQbit.PayrollSystem.git/OpenQbit.Payroll.BLL.BusinessService.Contracts/ICustomerManager.cs
@@ -0,0 +1,19 @@
+﻿using OpenQbit.Payroll.Common.Models.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQbit.Payroll.BLL.BusinessService.Contracts
+{
+    public interface ICustomerManager
+    {
+
+        bool RecordCustomer(Customer customer);
+        bool EditCustomer(Customer customer);
+        bool RemoveCustomer(Customer customer);
+        List<Customer> GetAllCustomer();
+        Customer FindAdvance(Customer customer);
+    }
+}
