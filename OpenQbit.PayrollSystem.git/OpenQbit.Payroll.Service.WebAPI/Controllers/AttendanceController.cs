40e5d1a5155ab2690f3efc0d51ca1dde793b8dd0
Rasith Upamal <rasithupamal@yahoo.com>
2017-07-07 14:57:13 +0530

    Test 09

---
+++ OpenQbit.PayrollSystem.git/OpenQbit.Payroll.Service.WebAPI/Controllers/AttendanceController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using OpenQbit.Payroll.Service.WebAPI.Models.API;
+
+namespace OpenQbit.Payroll.Service.WebAPI.Controllers
+{
+    public class AttendanceController : ApiController
+    {
+     //   AttendanceController GetAttendance()
+    //    {
+          //  APIAttendance attendance = new APIAttendance(id:12);
+          //  return attendance;
+  //      }
+    }
+}
